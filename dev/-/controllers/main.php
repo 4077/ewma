43cc82bd7a75c85e52b92aa6c938d612b710962b
fvzsf65536 <fed65536@yandex.ru>
2017-12-22 06:56:28 +0300

    createModule2

--- dev/-/controllers/main.php
+++ dev/-/controllers/main.php
@@ -2,6 +2,83 @@
 
 class Main extends \Controller
 {
+    public function createModule2()
+    {
+        if ($path = $this->data('path')) {
+            $pathArray = p2a($path);
+            $newPathArray = $pathArray;
+
+            $baseModule = $this->app->modules->getRootModule();
+
+            $baseModulePathArray = [];
+            foreach ($pathArray as $name) {
+                $baseModulePathArray[] = $name;
+
+                if ($module = $this->app->modules->getByPath(a2p($baseModulePathArray))) {
+                    $baseModule = $module;
+
+                    array_shift($newPathArray);
+                }
+            }
+
+            $newModuleNamespace = $this->data('ns') ? $this->data['ns'] : $baseModule->namespace;
+
+            $type = 'master';
+
+            if ($baseModule->type == 'slave') {
+                $type = 'slave';
+            }
+
+            if ($this->dataHas('master')) {
+                $type = 'master';
+            }
+
+            if ($this->dataHas('slave')) {
+                $type = 'slave';
+            }
+
+            $report = [];
+
+            $fullPathArray = p2a($baseModule->path);
+
+            foreach ($newPathArray as $newModuleName) {
+                if (is_numeric(substr($newModuleName, 0, 1))) {
+                    $newModuleName = '_' . $newModuleName;
+                }
+
+                $fullPathArray[] = $newModuleName;
+                $fullPath = a2p($fullPathArray);
+
+                if (!$this->data('ns')) {
+                    $newModuleNamespace .= '\\' . $newModuleName;
+                }
+
+                $content = '<?php return [';
+                $content .= PHP_EOL . "    'namespace' => '" . trim_l_backslash(str_replace('/', '\\', $newModuleNamespace)) . "'";
+
+                if ($type == 'slave') {
+                    $content .= "," . PHP_EOL . "    'type'      => 'slave'";
+                }
+
+                $content .= PHP_EOL . "];" . PHP_EOL;
+
+                write(abs_path('modules', $fullPath) . '/settings.php', $content);
+
+                $report[] = [
+                    'type' => $type,
+                    'path' => $fullPath,
+                    'ns'   => $newModuleNamespace
+                ];
+            }
+
+            if ($this->data('reset')) {
+                $report[] = $this->c('\ewma\cache~:reset');
+            }
+
+            return $report;
+        }
+    }
+
     public function createModule()
     {
         if ($newModulePath = $this->data('path')) {
