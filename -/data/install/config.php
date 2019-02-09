8ca309c8cece5e5cdaa53c90782d369c69187d5b
fed <platinumaccount@yandex.ru>
2019-02-09 03:37:09 +0300

    install data fix (default config)

--- -/data/install/config.php
+++ -/data/install/config.php
@@ -1,14 +1,11 @@
 <?php return [
-    'namespace' => 'ewma',
-    'config'    => [
-        'eventDispatchers' => [
-            '/eventDispatcher'
-        ],
-        'routers'          => [
-            // [path/to/router/controller => use html wrapper]
-            '\ewma\remoteCall router' => false,
-            '\ewma router'            => true,
-            '\ewma\routers router'    => false
-        ]
+    'eventDispatchers' => [
+        '/eventDispatcher'
+    ],
+    'routers'          => [
+        // [path/to/router/controller => use html wrapper]
+        '\ewma\remoteCall router' => false,
+        '\ewma router'            => true,
+        '\ewma\routers router'    => false
     ]
 ];
--- settings.php
+++ settings.php
@@ -1,14 +1,3 @@
 <?php return [
-    'namespace' => 'ewma',
-    'config'    => [
-        'eventDispatchers' => [
-            '/eventDispatcher'
-        ],
-        'routers'          => [
-            // [path/to/router/controller => use html wrapper]
-            '\ewma\remoteCall router' => false,
-            '\ewma router'            => true,
-            '\ewma\routers router'    => false
-        ]
-    ]
+    'namespace' => 'ewma'
 ];
