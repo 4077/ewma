092659af41b3692c3540ec77f58e792332b0ac80
fed <platinumaccount@yandex.ru>
2019-02-09 03:50:29 +0300

    \ewma\controllers\Router fix

--- -/controllers/router.php
+++ -/controllers/router.php
@@ -6,20 +6,19 @@ class Router extends \Controller implements RouterInterface
 {
     public function getResponse()
     {
-        $this->route('dev/cp')->to('\dev\cp~:view');
+        $this->route('dev/modules')->to('\ewma\dev\ui~:view');
         $this->route('dev/routers')->to('\ewma\routers\ui~:view');
-        $this->route('dev/modules')->to('\dev~:view');
         $this->route('dev/call-center')->to('\ewma\callCenter~:view');
         $this->route('dev/data-sets')->to('\ewma\dataSets~:view');
-        $this->route('dev/access/*')->to('\ewma\access router:getResponse');
         $this->route('dev/handlers')->to('\ewma\handlers\ui~:view');
-        $this->route('dev/routers')->to('\ewma\routers\ui~:view');
+        $this->route('dev/components')->to('\ewma\components\ui~:view');
+        $this->route('dev/access/*')->to('\ewma\access router:getResponse');
 
         $this->route('dev/cache-reset')->to('\ewma~cache:reset', [], function ($response) {
             return implode('<br>', $response);
         });
 
-        $this->route('login')->to('\std\auth login:view');
+        $this->route('login')->to('\std\ui\auth login:view');
 
         return $this->routeResponse();
     }
